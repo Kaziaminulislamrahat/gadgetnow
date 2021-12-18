51235ff39eeb5cfb2edf09baebe6c1db18d69e9c
Kaziaminulislamrahat <info@kaziaminulislam.com>
2021-12-19 01:59:33 +0600

    joint two list
    create a dictionary and joint 2 list.
    scrape specification table. required html tag remove.

--- smartwatchspex.py
+++ smartwatchspex.py
@@ -3,15 +3,26 @@ from bs4 import BeautifulSoup as bs
 
 url="https://www.smartwatchspex.com/matrix-powerwatch-2-luxe/"
 response=requests.get(url)
-soup=bs(response.text,"lxml")
+soup=bs(response.text,"html.parser")
 #title scrape
 title=soup.find("h1", {"class":"aps-main-title"}).text
 print(title)
-#table data scrape
-table_spec=[]
-table_h3=soup.find_all("h3", {"class":"aps-group-title"})
-test_table=table_spec.extend(table_h3)
-table_raw=table_spec.remove('<h3 class="aps-group-title">')
-table_final=table_spec.extend(table_raw) 
-for h3 in table_spec:
-    print(h3)
+#table Left data scrape
+table_left=[]
+table_h3_a=soup.find_all("strong", {"class":"aps-term"})
+test_table_a=table_left.extend(table_h3_a)
+#print(table_left)
+#for h3 in table_left:
+    #print(h3)
+#table Left data scrape
+table_right=[]
+table_h3_b=soup.find_all("span", {"class":"aps-1co"})
+test_table_b=table_right.extend(table_h3_b)
+#print(table_right)
+#two list merge with a dictionary
+merge_data=dict(zip(table_left, table_right))
+print(merge_data)
+
+
+
+
