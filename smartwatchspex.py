255ec184e36d4da41b13fa0e3bb6b04cd7fc3742
Kaziaminulislamrahat <info@kaziaminulislam.com>
2021-12-20 02:09:28 +0600

    smartwatchspex table scrape
    Scrape All data from table. Here buy now avoided. Now i can format my content from here

--- smartwatchspex.py
+++ smartwatchspex.py
@@ -1,5 +1,6 @@
 import requests
-from bs4 import BeautifulSoup as bs 
+from bs4 import BeautifulSoup as bs
+import csv 
 
 url="https://www.smartwatchspex.com/matrix-powerwatch-2-luxe/"
 response=requests.get(url)
@@ -9,20 +10,27 @@ title=soup.find("h1", {"class":"aps-main-title"}).text
 print(title)
 #table Left data scrape
 table_left=[]
-table_h3_a=soup.find_all("strong", {"class":"aps-term"})
-test_table_a=table_left.extend(table_h3_a)
-#print(table_left)
-#for h3 in table_left:
+
+for h3 in soup.find_all("strong",{"class":"aps-term"}):
+    table_left.extend(h3)
+    #table_left.pop(0)
+    #table_left.extend(h3)
     #print(h3)
 #table Left data scrape
 table_right=[]
-table_h3_b=soup.find_all("span", {"class":"aps-1co"})
-test_table_b=table_right.extend(table_h3_b)
-#print(table_right)
+for h4 in soup.find_all("span",{"class":"aps-1co"}):
+    table_right.extend(h4)
+    #table_right.pop(0)
+    #table_right.extend(h4)
+
+#print(table_right,table_left)
 #two list merge with a dictionary
-merge_data=dict(zip(table_left, table_right))
-print(merge_data)
+merge_data=dict(zip(table_left,table_right))
+del merge_data['Buy']
+#print(merge_data)
 
 
-
-
+with open("kazikalko14.csv", "a+", newline="",encoding=("utf8")) as f:
+	writer = csv.writer(f)
+	writer.writerow(merge_data)
+	#writer.writerow(table_right)
