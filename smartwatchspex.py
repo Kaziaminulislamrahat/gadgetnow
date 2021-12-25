9bf6bcb11137ee4161ed20801a275afcaa6ddc7e
Kaziaminulislamrahat <info@kaziaminulislam.com>
2021-12-25 09:49:02 +0600

    general+display+body
    key not found problem solve

--- smartwatchspex.py
+++ smartwatchspex.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup as bs
 import csv 
 import random
 
-url="https://www.smartwatchspex.com/xiaomi-mi-band-hrx-edition-specs/"
-response=requests.get(url)
+url="https://www.smartwatchspex.com/amazfit-verge-lite/"
+headers = {'user-agent': 'my-app/0.0.1'}
+response=requests.get(url,headers=headers)
 soup=bs(response.text,"html.parser")
 #title scrape
 title=soup.find("h1", {"class":"aps-main-title"}).text
@@ -26,29 +27,30 @@ for h4 in soup.find_all("span",{"class":"aps-1co"}):
 
 #print(table_right,table_left)
 #two list merge with a dictionary
-merge_data=dict(zip(table_left,table_right))
-del merge_data['Buy']
+merge_list=dict(zip(table_left,table_right))
+del merge_list['Buy']
+brand=soup.find("span",attrs={"class":"aps-product-brand"}).text
+brand_dict={}
+brand_dict["brandname"]=brand.replace("Brand:","")
+merge_data={**merge_list, **brand_dict}
+
+for key, value in merge_data.items():
+      if value == '-':
+           merge_data[key] = ' information not available'
 #dictionary data access single item
 #test_dict=merge_data["Chipset"]
-#print(test_dict)
+#print(merge_data)
 #test gadgetnow style content create, here test first paragraph
 #p1="This " + merge_data["Device Name"] +" is a unique combination of style and functionality that keeps you ahead of time with its unique features."
 start_1 = ["This", "The", "Do you know", "You know"]
 start_2 = ["unique ", "exclusive ", "different ", "great "]
 
-m_p2= random.choice(start_1) +" "+ merge_data["Device Name"] +" is a " + random.choice(start_2) + "style and functionality that keeps you ahead of time with its "+random.choice(start_2)+"features."
+m_first_sentence= random.choice(start_1) +" "+ merge_data["Device Name"] +" is a " + random.choice(start_2) + "style and functionality that keeps you ahead of time with its "+random.choice(start_2)+"features."
+m_general= merge_data["Device Name"] + " release date was "+ merge_data["Release"]+"." + merge_data["Ideal for (Gender)"] + " can wear this "+ merge_data["Device Type"]+"."
+m_display= merge_data["Device Name"] + " has "+ merge_data["Screen Size"]+ " "+ merge_data["Type"]+ " display"+ ".It's screen resolution is "+ merge_data["Screen Resolution"]+ " "+ "and pixel density"+ merge_data["Pixel Density"]+ ".For screen protection, you will find here "+ merge_data["Screen Protection"]+"."
+m_body= merge_data["Device Name"] + " "+ merge_data["Device Type"]+ " is "+merge_data["Shape"]+ " shape."+merge_data["brandname"]+" gives "+merge_data["Case/Bezel"]+ " and, it's case dimensions are "+ merge_data["Case Dimensions"]+ "."+ merge_data["Strap Material"]+ " strap and it's size is "+ merge_data["Strap/Band Size"]+".Only "+merge_data["Colors"]+ " variant available. "+merge_data["Device Name"]+" weight is "+merge_data["Weight"]+"."
+#m_release_date= merge_data["Device Name"] + " release date was "+ merge_data["Release"]
+#m_release_date= merge_data["Device Name"] + " release date was "+ merge_data["Release"]
 
 
-#m_p3=
-
-
-print(m_p2)
-
-
-
-
-#export as a csv file
-'''with open("kazikalko14.csv", "a+", newline="",encoding=("utf8")) as f:
-	writer = csv.writer(f)
-	writer.writerow(merge_data)
-	#writer.writerow(table_right)'''
+print(m_first_sentence,m_general,m_display,m_body)
