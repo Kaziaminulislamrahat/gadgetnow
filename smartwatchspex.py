ea9561d0e1805236ded93fd4b32706b1cd5f4d8d
Kaziaminulislamrahat <info@kaziaminulislam.com>
2021-12-29 03:13:07 +0600

    Network,memory,platform
    content generate 3 field

--- smartwatchspex.py
+++ smartwatchspex.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 import csv 
 import random
 
-url="https://www.smartwatchspex.com/amazfit-verge-lite/"
+url="https://www.smartwatchspex.com/apple-watch-38mm-1st-gen-specifications/"
 headers = {'user-agent': 'my-app/0.0.1'}
 response=requests.get(url,headers=headers)
 soup=bs(response.text,"html.parser")
@@ -47,11 +47,22 @@ start_1 = ["This", "The", "Do you know", "You know"]
 start_2 = ["unique ", "exclusive ", "different ", "great "]
 
 m_first_sentence= random.choice(start_1) +" "+ merge_data.get("Device Name","None") +" is a " + random.choice(start_2) + "style and functionality that keeps you ahead of time with its "+random.choice(start_2)+"features."
-m_general= merge_data.get("Device Name","none") + " release date was "+ merge_data.get("Release","none")+"." + merge_data.get("Ideal for (Gender)","none") + " can wear this "+ merge_data.get("Device Type","none")+"."
+m_general= merge_data.get("Device Name","none") + " release date was "+ merge_data.get("Release","none")+"." + merge_data.get("Ideal for (Gender)","everybody") + " can wear this "+ merge_data.get("Device Type","none")+"."
 m_display= merge_data["Device Name"] + " has "+ merge_data.get("Screen Size","none")+ " "+ merge_data.get("Type","none")+ " display"+ ".It's screen resolution is "+ merge_data.get("Screen Resolution","None")+ " "+ "and pixel density"+ merge_data.get("Pixel Density","none")+ ".For screen protection, you will find here "+ merge_data.get("Screen Protection","none")+"."
 m_body= merge_data.get("Device Name","none") + " "+ merge_data.get("Device Type","none")+ " is "+merge_data.get("Shape","none")+ " shape."+merge_data.get("brandname","none")+" gives "+merge_data.get("Case/Bezel","none")+ " and, it's case dimensions are "+ merge_data.get("Case Dimensions","none")+ "."+ merge_data.get("Strap Material","none")+ " strap and it's size is "+ merge_data.get("Strap/Band Size","none")+"."+merge_data.get("Colors","none")+ " variant available. "+merge_data.get("Device Name","none")+" weight is "+merge_data.get("Weight","none")+"."
-m_platform= merge_data.get("brandname","none") + " used "+ merge_data.get("Operating System","none")
-#m_release_date= merge_data["Device Name"] + " release date was "+ merge_data["Release"]
+m_platform= merge_data.get("brandname","none") + " used "+ merge_data.get("Operating System","none")+"." + merge_data.get("brandname","none")+ " used "+ merge_data.get("Chipset","none") +" chipset."+ merge_data.get("CPU","none") +" CPU and "+ merge_data.get("GPU","none")+" used for this "+merge_data.get("Device Type","none")+ "."
+m_memory= merge_data.get("Device Name","None") + " has "+ merge_data.get("RAM","none")+" ram."+ merge_data.get("brandname","None")+ " provides "+ merge_data.get("Internal Memory","no internal memory.")+ " internal memory and "+ merge_data.get("SD Card Slot","no extra memory card you can use.")+" you can use extra memory card."
+m_network_conectivity= merge_data.get("brandname","None") + " offer "+ merge_data.get("Sim","none")+" sim card."+ merge_data.get("Device Name","None")+ " has "
 
 
-print(m_first_sentence,m_general,m_display,m_body,m_platform)
+
+
+
+
+
+
+
+
+
+
+print(m_first_sentence,"\n",m_general,"\n",m_display,"\n",m_body,"\n",m_platform,"\n",m_memory,"\n",m_network_conectivity)
