4e3fe32aa91fad8ad3947906a02ccbb5f5ba5220
Kaziaminulislamrahat <info@kaziaminulislam.com>
2021-12-28 09:02:13 +0600

    platform add
    Platform add newly but some data missing so same way solve required as like key missing.

--- smartwatchspex.py
+++ smartwatchspex.py
@@ -33,7 +33,7 @@ brand=soup.find("span",attrs={"class":"aps-product-brand"}).text
 brand_dict={}
 brand_dict["brandname"]=brand.replace("Brand:","")
 merge_data={**merge_list, **brand_dict}
-
+#print(merge_data)
 for key, value in merge_data.items():
       if value == '-':
            merge_data[key] = ' information not available'
@@ -49,9 +49,9 @@ start_2 = ["unique ", "exclusive ", "different ", "great "]
 m_first_sentence= random.choice(start_1) +" "+ merge_data["Device Name"] +" is a " + random.choice(start_2) + "style and functionality that keeps you ahead of time with its "+random.choice(start_2)+"features."
 m_general= merge_data["Device Name"] + " release date was "+ merge_data["Release"]+"." + merge_data["Ideal for (Gender)"] + " can wear this "+ merge_data["Device Type"]+"."
 m_display= merge_data["Device Name"] + " has "+ merge_data["Screen Size"]+ " "+ merge_data["Type"]+ " display"+ ".It's screen resolution is "+ merge_data["Screen Resolution"]+ " "+ "and pixel density"+ merge_data["Pixel Density"]+ ".For screen protection, you will find here "+ merge_data["Screen Protection"]+"."
-m_body= merge_data["Device Name"] + " "+ merge_data["Device Type"]+ " is "+merge_data["Shape"]+ " shape."+merge_data["brandname"]+" gives "+merge_data["Case/Bezel"]+ " and, it's case dimensions are "+ merge_data["Case Dimensions"]+ "."+ merge_data["Strap Material"]+ " strap and it's size is "+ merge_data["Strap/Band Size"]+".Only "+merge_data["Colors"]+ " variant available. "+merge_data["Device Name"]+" weight is "+merge_data["Weight"]+"."
-#m_release_date= merge_data["Device Name"] + " release date was "+ merge_data["Release"]
+m_body= merge_data["Device Name"] + " "+ merge_data["Device Type"]+ " is "+merge_data["Shape"]+ " shape."+merge_data["brandname"]+" gives "+merge_data["Case/Bezel"]+ " and, it's case dimensions are "+ merge_data["Case Dimensions"]+ "."+ merge_data["Strap Material"]+ " strap and it's size is "+ merge_data["Strap/Band Size"]+"."+merge_data["Colors"]+ " variant available. "+merge_data["Device Name"]+" weight is "+merge_data["Weight"]+"."
+m_platform= merge_data["brandname"] + " used "+ merge_data["Operating System"]
 #m_release_date= merge_data["Device Name"] + " release date was "+ merge_data["Release"]
 
 
-print(m_first_sentence,m_general,m_display,m_body)
+print(m_first_sentence,m_general,m_display,m_body,m_platform)
